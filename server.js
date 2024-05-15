8bf8dcc00a9f091bd0c87c66232f1143c20c529d
이소정 <esomnmm@gmail.com>
2024-05-15 23:52:26 +0900

    set ORM(sequelize), mysql2

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const { Sequelize } = require("sequelize");
+
+const { Sequelize, Op, Model, DataTypes } = require("sequelize");
+
+/* set server */
 const app = express();
-const port = process.env.EXPRESS_SERVER_PORT || 3000;
+const port = process.env.EXPRESS_SERVER_PORT || 8080;
 
 app.get("/", (req, res) => {
   res.json({
@@ -12,3 +17,26 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`server is listening at localhost:${port}`);
 });
+
+/* set ORM */
+const sequelize = new Sequelize("database", "username", "password", {
+  host: "localhost",
+  dialect: "mysql",
+  logging: (...msg) => console.log(msg), // Displays all log function call parameters
+});
+
+// Test the connection
+sequelize
+  .authenticate()
+  .then((res) => {
+    console.log("Connection has been established successfully.", res);
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
+
+// Closing the connection
+sequelize
+  .close()
+  .then(() => {})
+  .catch((error) => {});
